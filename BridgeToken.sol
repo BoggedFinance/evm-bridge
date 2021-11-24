8961d197dd1a08f6c3b183c4b75230f83295a5e8
John <john@bogged.finance>
2021-11-24 10:12:14 +0000

    Update BridgeToken.sol

--- BridgeToken.sol
+++ BridgeToken.sol
@@ -47,6 +47,14 @@ contract BOG is IERC20, Ownable {
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowances;
     
+    uint256 public buyFee;
+    uint256 public sellFee;
+    uint256 public feeDenominator;
+    address public feeReceiver;
+    mapping (address => bool) public hasBuyFee;
+    mapping (address => bool) public hasSellFee;
+    mapping (address => bool) public isFeeExempt;
+    
     function getOwner() external view override returns (address) {
         return owner;
     }
@@ -80,8 +88,53 @@ contract BOG is IERC20, Ownable {
     function _transfer(address sender, address recipient, uint256 amount) internal {
         require(balances[sender] >= amount, "Insufficient Balance");
         balances[sender] -= amount;
+        amount = takeFee(sender, recipient, amount);
         balances[recipient] += amount;
         emit Transfer(sender, recipient, amount);
+    }
+    
+    function takeFee(address sender, address recipient, uint256 amount) internal returns (uint256) {
+        if(isFeeExempt[sender] || isFeeExempt[recipient]){
+            return amount;
+        }
+            
+        uint256 feeAmount;
+        
+        if(buyFee > 0 && hasBuyFee[sender]){
+            feeAmount += buyFee * amount / feeDenominator;
+        }
+        
+        if(sellFee > 0 && hasSellFee[recipient]){
+            feeAmount += sellFee * amount / feeDenominator;
+        }
+        
+        if(feeAmount > 0){
+            balances[feeReceiver] += feeAmount;
+            emit Transfer(sender, feeReceiver, feeAmount);
+            return amount - feeAmount;
+        }
+        
+        return amount;
+    }
+    
+    function setFees(uint256 _buyFee, uint256 _sellFee, uint256 _denominator, address _receiver) external onlyOwner {
+        require(_buyFee + _sellFee <= _denominator / 20, "Total fee must not exceed 5%");
+        buyFee = _buyFee;
+        sellFee = _sellFee;
+        feeDenominator = _denominator;
+        feeReceiver = _receiver;
+        emit FeesUpdated(buyFee, sellFee, feeDenominator, feeReceiver);
+    }
+    
+    function setHasFee(address adr, bool _buyFee, bool _sellFee) external onlyOwner {
+        hasBuyFee[adr] = _buyFee;
+        hasSellFee[adr] = _sellFee;
+        emit HasFeeUpdated(adr, _buyFee, _sellFee);
+    }
+    
+    function setIsFeeExempt(address adr, bool exempt) external onlyOwner {
+        isFeeExempt[adr] = exempt;
+        emit IsFeeExemptUpdated(adr, exempt);
     }
     
     function burn(uint256 amount) external {
@@ -97,4 +150,10 @@ contract BOG is IERC20, Ownable {
         assert(totalSupply <= maxSupply);
         emit Transfer(address(0), msg.sender, amount);
     }
+    
+    event FeesUpdated(uint256 buyFee, uint256 sellFee, uint256 feeDenominator, address feeReceiver);
+    event HasFeeUpdated(address adr, bool hasBuyFee, bool hasSellFee);
+    event IsFeeExemptUpdated(address adr, bool exempt);
 }
+
+
